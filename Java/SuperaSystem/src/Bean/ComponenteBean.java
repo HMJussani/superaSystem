0c5ed62671d31797e705d9b3728df7791a06848e
Henrique M. Jussani <hmjussani@gmail.com>
2023-09-14 23:58:11 -0300

    Add files via upload

---
+++ Java/SuperaSystem/src/Bean/ComponenteBean.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+/**
+ *
+ * @author hmjussani
+ */
+public class ComponenteBean {
+
+    private int idComp;
+    private String tipo;
+    private String fabricante;
+    private String modelo;
+    private String capacidade;
+    private String detalhe;
+
+    public int getIdComp() {
+        return idComp;
+    }
+
+    public void setIdComp(int idComp) {
+        this.idComp = idComp;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(String capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public String getDetalhe() {
+        return detalhe;
+    }
+
+    public void setDetalhe(String detalhe) {
+        this.detalhe = detalhe;
+    }
+
+    
+    
+}
